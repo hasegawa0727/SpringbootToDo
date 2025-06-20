5159d232f67a48a08be62a799a37a7e127b0d8b6
itf <itf@DESKTOP-5AISMI7>
2025-06-20 11:45:43 +0900

    Complete Lesson

--- SpringbootToDo/src/main/java/com/example/demo1/ToDo.java
+++ SpringbootToDo/src/main/java/com/example/demo1/ToDo.java
@@ -1,17 +1,25 @@
 package com.example.demo1;
 
+import java.time.LocalDateTime;
+
 public class ToDo {
 	private static int count = 1;
 	
 	private final long id;
 	private String title;
 	private String status;
+	private String description;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
 
 	
-	public ToDo(String title) {
+	public ToDo(String title, String description) {
 		this.id = count++;
 		this.title = title;
 		this.status = "未着手";
+		this.description = description;
+		this.createdAt = LocalDateTime.now();
+		this.updatedAt = LocalDateTime.now();
 	}
 	
 	public long getId() {
@@ -34,4 +42,55 @@ public class ToDo {
 		this.status = status;
 	}
 
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
 }
+
+/*以下Lombokでの定義
+ * (パッケージ省略)
+ * import lombok.Getter;
+ * import lombock.Setter;
+ * import lombok.NoArgsConstructor;
+ * 
+ * import java.time.LocalDateTime;
+ * 
+ * @Getter
+ * @Setter
+ * @NoArgsConstructor
+ * public class ToDo {
+ * 	private static int count = 1;
+ * 
+ * 	private final long id = count++;
+ * 	private String title;
+ * 	private String status = "未着手";
+ * 	private String description;
+ * 	private LocalDateTime createdAt = LocalDateTime.now();
+ * 	private LocalDateTime updatedAt = LocalDateTime.now();
+ *  
+ * 	public ToDo(String title, String description) {
+ * 		this.title = title;
+ * 		this.description = description;
+ *  }
+ */ 
--- SpringbootToDo/src/main/java/com/example/demo1/ToDoController.java
+++ SpringbootToDo/src/main/java/com/example/demo1/ToDoController.java
@@ -1,24 +1,32 @@
 package com.example.demo1;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/todos")
 public class ToDoController {
+
+    private final SpringbootToDoApplication springbootToDoApplication;
 	private List<ToDo> list = new ArrayList<>();
 	
-	public ToDoController() {
-		list.add(new ToDo("買い物に行く"));
-		list.add(new ToDo("本を読む"));
+	public ToDoController(SpringbootToDoApplication springbootToDoApplication) {
+		list.add(new ToDo("買い物に行く", "夕飯用の食材を買いに行く"));
+		list.add(new ToDo("本を読む", "溜まっている本を消化する"));
+		this.springbootToDoApplication = springbootToDoApplication;
 	}
 	
 	@GetMapping
@@ -28,30 +36,72 @@ public class ToDoController {
 	
 	@GetMapping("/{id}")
 	public ToDo getTodo(@PathVariable long id) {
-		long index = id -1;
+		int index = (int)(id -1);
 		
 		if(index < 0 || index >= list.size()) {
-			throw new RuntimeException("指定されたIDのToDoは存在しません： " + id);
+			throw new WrongIdException(id);
 		}
-		return list.get((int)index);
+		return list.get(index);
+	}
+	
+	@GetMapping("/search")
+	public List<ToDo> searchTodo(@RequestParam String body) {
+		return list.stream()
+				.filter(todo -> todo.getTitle().contains(body) || 
+						todo.getStatus().contains(body) ||
+						todo.getDescription().contains(body))
+				.collect(Collectors.toList());
+	}
+	
+	@GetMapping("/sort")
+	public List<ToDo> sortTodo() {
+		return list.stream()
+				.sorted(Comparator.comparing((ToDo todo) -> "完了".equals(todo.getStatus()) ? 1: 0)
+				.thenComparing((ToDo todo) -> todo.getId()))
+				.collect(Collectors.toList());
 	}
 	
 	@PostMapping
 	public ToDo postTodo(@RequestBody ToDo newtodo) {
+		newtodo.setCreatedAt(LocalDateTime.now());
+		newtodo.setUpdatedAt(LocalDateTime.now());
 		list.add(newtodo);
 		return newtodo;
 	}
 	
 	@DeleteMapping("/{id}")
 	public String deleteTodo(@PathVariable long id) {
-		long index = id - 1;
+		int index = (int)(id - 1);
 		
 		if(index < 0 || index > list.size()) {
-			throw new RuntimeException("指定されたIDのToDoは存在しません： " + id);
+			throw new WrongIdException(id);
 		}
-		list.remove((int)index);
+		list.remove(index);
 		return "ID" + id + "のToDOを削除しました";
 	}
 	
+	@PatchMapping("/{id}")
+	public ToDo patchTodo(@PathVariable long id, @RequestBody ToDo patchTodo) {
+		int index = (int)(id - 1);
+		
+		if(index < 0 || index > list.size()) {
+			throw new WrongIdException(id);
+		}
+		
+		ToDo existing = list.get(index);
+		
+		if(patchTodo.getTitle() != null) {
+			existing.setTitle(patchTodo.getTitle() );
+		}
+		if(patchTodo.getDescription() != null) {
+			existing.setDescription(patchTodo.getDescription() );
+		}
+		if(patchTodo.getStatus() != null) {
+			existing.setStatus(patchTodo.getStatus() );
+		}
+		
+		existing.setUpdatedAt(LocalDateTime.now());
+		return existing;
+	}
 
 }
